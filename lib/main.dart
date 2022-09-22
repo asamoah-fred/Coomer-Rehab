d99a007834bcf31ea749d00d4e40f8149ffd7c40
asamoah-fred <asamoahgodfred42@gmail.com>
2022-09-22 11:14:02 +0000

    added login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:coomer_rehab/pages/login-screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,12 +10,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: 
+      home: LogInScreen(),
     );
   }
 }
-
---
+++ lib/pages/login-screen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatefulWidget {
+  const LogInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LogInScreen> createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
